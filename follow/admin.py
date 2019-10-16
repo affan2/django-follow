5fde9b160f4225d1445bdabf544e54fbad7d00db
Mohammad Abouchama <mohammadabouchama@gmail.com>
2019-10-16 13:39:34 +0400

    Disable addition and deletion from the back-end

--- follow/admin.py
+++ follow/admin.py
@@ -2,4 +2,12 @@ from .models import Follow
 from django.contrib import admin
 
 
-admin.site.register(Follow)
+class FollowAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def get_readonly_fields(self, request, obj=None):
+        return [f.name for f in self.model._meta.fields]
+
+
+admin.site.register(Follow, FollowAdmin)
